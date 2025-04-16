f846aa900c60ec45bb31323f0b58c47b48fab5f5
Yunxin119 <1999yunxin@gmail.com>
2025-04-15 23:12:53 -0700

    first commit: migrate from previous repository - (previous repository crashed due to ambiguous in XCode setup)

---
+++ LifeSignalV2Watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LifeSignalV2Watch Watch App
+//
+//  Created by Yunxin Liu on 4/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LifeSignalV2Watch Watch App/LifeSignalV2WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeSignalV2WatchApp.swift
+//  LifeSignalV2Watch Watch App
+//
+//  Created by Yunxin Liu on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct LifeSignalV2Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
